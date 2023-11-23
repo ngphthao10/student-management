16de657b3090435104e6276fb5343a19a894d2ad
ngphthao28 <ngphthao031028@gmail.com>
2023-11-24 00:31:22 +0700

    first commit

---
+++ src/main/java/Model/StudentManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Model;
+
+/**
+ *
+ * @author PHUONG THAO
+ */
+public class StudentManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
