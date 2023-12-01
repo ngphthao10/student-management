cbf7f6657e8aaae74dae5135faf9d6abf4bfdeb0
ngphthao10 <n21dccn078@student.ptithcm.edu.vn>
2023-12-02 00:49:02 +0700

    some changes :)))

--- src/main/java/View/HomePage.java
+++ src/main/java/View/HomePage.java
@@ -32,6 +32,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton14 = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu = new javax.swing.JMenu();
         mnThongTin = new javax.swing.JMenu();
@@ -45,8 +46,8 @@ public class HomePage extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(209, 232, 195));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-user-64.png")); // NOI18N
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1421, 6, -1, -1));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-user-64.png"))); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1450, 0, -1, -1));
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(0, 0, 0));
@@ -65,15 +66,15 @@ public class HomePage extends javax.swing.JFrame {
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel6.setText("XX - Ten người dùng");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(1174, 17, 241, -1));
+        jLabel6.setText("XX1 - Ten người dùng");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(1180, 20, 241, -1));
 
         jSeparator1.setBackground(new java.awt.Color(153, 153, 255));
         jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
         jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(209, 6, 6, 64));
 
         btOut.setBackground(new java.awt.Color(209, 232, 195));
-        btOut.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-cat-64.png")); // NOI18N
+        btOut.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-cat-64.png"))); // NOI18N
         btOut.setBorder(null);
         jPanel1.add(btOut, new org.netbeans.lib.awtextra.AbsoluteConstraints(1, 0, -1, 76));
 
@@ -85,7 +86,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton1.setBackground(new java.awt.Color(76, 124, 97));
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-imac-settings-48.png")); // NOI18N
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-elective-48.png"))); // NOI18N
         jButton1.setText(" Chương trình đào tạo  ");
         jButton1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jButton1.setHideActionText(true);
@@ -96,7 +97,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton7.setBackground(new java.awt.Color(76, 124, 97));
         jButton7.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton7.setForeground(new java.awt.Color(255, 255, 255));
-        jButton7.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-department-48.png")); // NOI18N
+        jButton7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-department-48.png"))); // NOI18N
         jButton7.setText(" Danh sách khoa, ngành");
         jButton7.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jButton7.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
@@ -106,7 +107,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton8.setBackground(new java.awt.Color(76, 124, 97));
         jButton8.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton8.setForeground(new java.awt.Color(255, 255, 255));
-        jButton8.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-diploma-48.png")); // NOI18N
+        jButton8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-report-card-48.png"))); // NOI18N
         jButton8.setText(" Chi tiết bảng điểm        ");
         jButton8.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jButton8.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
@@ -116,7 +117,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton9.setBackground(new java.awt.Color(76, 124, 97));
         jButton9.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton9.setForeground(new java.awt.Color(255, 255, 255));
-        jButton9.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-report-card-48.png")); // NOI18N
+        jButton9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-assignment-48.png"))); // NOI18N
         jButton9.setText(" Phân công giảng dạy    ");
         jButton9.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jButton9.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
@@ -126,7 +127,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton10.setBackground(new java.awt.Color(76, 124, 97));
         jButton10.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton10.setForeground(new java.awt.Color(255, 255, 255));
-        jButton10.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-register-48.png")); // NOI18N
+        jButton10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-register-48.png"))); // NOI18N
         jButton10.setText(" Đăng ký lớp tín chỉ       ");
         jButton10.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jButton10.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
@@ -136,7 +137,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton11.setBackground(new java.awt.Color(76, 124, 97));
         jButton11.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton11.setForeground(new java.awt.Color(255, 255, 255));
-        jButton11.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-class-48.png")); // NOI18N
+        jButton11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-class-48.png"))); // NOI18N
         jButton11.setText(" Thông tin lớp học        ");
         jButton11.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jButton11.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
@@ -146,7 +147,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton12.setBackground(new java.awt.Color(76, 124, 97));
         jButton12.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton12.setForeground(new java.awt.Color(255, 255, 255));
-        jButton12.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-student-48.png")); // NOI18N
+        jButton12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-student-48.png"))); // NOI18N
         jButton12.setText(" Thông tin sinh viên     ");
         jButton12.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jButton12.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
@@ -156,7 +157,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton13.setBackground(new java.awt.Color(76, 124, 97));
         jButton13.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton13.setForeground(new java.awt.Color(255, 255, 255));
-        jButton13.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-teacher-48.png")); // NOI18N
+        jButton13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-teacher-48.png"))); // NOI18N
         jButton13.setText(" Thông tin giảng viên   ");
         jButton13.setToolTipText("");
         jButton13.setBorder(javax.swing.BorderFactory.createEtchedBorder());
@@ -167,7 +168,7 @@ public class HomePage extends javax.swing.JFrame {
         jButton14.setBackground(new java.awt.Color(76, 124, 97));
         jButton14.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton14.setForeground(new java.awt.Color(255, 255, 255));
-        jButton14.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\icons8-exit-48 (1).png")); // NOI18N
+        jButton14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icons8-exit-48 (1).png"))); // NOI18N
         jButton14.setText(" Thoát                             ");
         jButton14.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jButton14.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
@@ -180,9 +181,11 @@ public class HomePage extends javax.swing.JFrame {
 
         jLabel3.setBackground(new java.awt.Color(255, 255, 255));
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\PHUONG THAO\\OneDrive\\Documents\\NetBeansProjects\\StudentManagement\\Image\\6_innovation01 (2).jpg")); // NOI18N
         jLabel3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jPanel3.add(jLabel3);
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/6_innovation01 (1).jpg"))); // NOI18N
+        jPanel3.add(jLabel7);
 
         getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 80, 1220, 710));
 
@@ -209,8 +212,9 @@ public class HomePage extends javax.swing.JFrame {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                HomePage ctdt = new HomePage();
-                ctdt.setVisible(true);
+                HomePage hp = new HomePage();
+                hp.setVisible(true);
+                hp.setExtendedState(MAXIMIZED_BOTH);
             }
         });
     }
@@ -232,6 +236,7 @@ public class HomePage extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JMenu jMenu;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
